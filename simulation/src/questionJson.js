eb8d5ae972aeab526b2782c9a66a77ec5318979e
vleadadmin <admin@vlabs.ac.in>
2024-10-23 08:52:03 +0000

    https://virtual-labs.github.io/virtual-labs/exp-dead-weight-tester-coep click on the link to test your code.

---
+++ simulation/src/questionJson.js
@@ -0,0 +1,143 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "What is the range of pressure that a low-pressure gauge typically measures?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "0-60 psi"
+					}, {
+						"ANSID": false,
+						"content": "0-100 psi"
+					}, {
+						"ANSID": false,
+						"content": "0-30 psi"
+					},{
+						"ANSID": true,
+						"content": "0-15 psi"
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": " What is the most common unit of measurement used for pressure gauges?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Pascal"
+					}, {
+						"ANSID": false,
+						"content": "Bar "
+					}, {
+						"ANSID": true,
+						"content": "PSI"
+					}, {
+						"ANSID": false,
+						"content": "Atmosphere"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "What is the purpose of the glass cover on a pressure gauge?",
+					"ANS": [{
+						"ANSID": true,
+						"content": "To protect the dial face from damage"
+					}, {
+						"ANSID": false,
+						"content": "To provide a visual indication of pressure changes"
+					}, {
+						"ANSID": false,
+						"content": "To magnify the pressure reading"
+					}, {
+						"ANSID": false,
+						"content": "To protect the pointer from damage"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "What is the purpose of the connection on a pressure gauge?",
+					"ANS": [{
+						"ANSID": true,
+						"content": "To connect the gauge to the process being measured"
+					}, {
+						"ANSID": false,
+						"content": "To provide a visual indication of pressure changes"
+					}, {
+						"ANSID": false,
+						"content": "To protect the gauge from overpressure"
+					}, {
+						"ANSID": false,
+						"content": "None of these"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "What is the purpose of the movement in a pressure gauge?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "To provide a visual indication of pressure changes"
+					}, {
+						"ANSID": false,
+						"content": "To protect the gauge from overpressure"
+					}, {
+						"ANSID": true,
+						"content": "To convert pressure into a mechanical deflection"
+					}, {
+						"ANSID": false,
+						"content": "None of these"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "What is the purpose of the case on a pressure gauge?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "To provide a visual indication of pressure changes"
+					}, {
+						"ANSID": true,
+						"content": "To protect the gauge from overpressure"
+					}, {
+						"ANSID": false,
+						"content": "To hold the movement in place"
+					}, {
+						"ANSID": false,
+						"content": "To hold the bourdon tube in place"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
